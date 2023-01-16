6382dd85d3724a214c8e09c098fd5298d2020889
drregg6 <dave.r.regg@gmail.com>
2023-01-16 17:02:22 -0500

    assignment-02

---
+++ assignment-02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment-02
+//
+//  Created by Dave Regg on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
